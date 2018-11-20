5d1821169f715b5f0d41e7f903e2bc882c296d3e
Ibrahim <ibrahim@sawadogo.xyz>
2018-11-20 14:29:07 +0100

    ex 2 done

--- Allfiles/Mod02/Labfiles/Starter/Exercise 2/School/StudentForm.xaml.cs
+++ Allfiles/Mod02/Labfiles/Starter/Exercise 2/School/StudentForm.xaml.cs
@@ -20,13 +20,24 @@ namespace School
         // If the user clicks OK to save the Student details, validate the information that the user has provided
         private void ok_Click(object sender, RoutedEventArgs e)
         {
+            DateTime value;
             // TODO: Exercise 2: Task 2a: Check that the user has provided a first name
-            // TODO: Exercise 2: Task 2b: Check that the user has provided a last name
-            // TODO: Exercise 2: Task 3a: Check that the user has entered a valid date for the date of birth
-            // TODO: Exercise 2: Task 3b: Verify that the student is at least 5 years old
+            //if (this.firstName.Text.Length.Equals(0)) { }
+            if (string.IsNullOrWhiteSpace(this.firstName.Text)) { MessageBox.Show("The student must have a first name", "Error", MessageBoxButton.OK, MessageBoxImage.Error); return; }
 
+            // TODO: Exercise 2: Task 2b: Check that the user has provided a last name
+            if (string.IsNullOrWhiteSpace(this.lastName.Text)) { MessageBox.Show("The student must have a last name", "Error", MessageBoxButton.OK, MessageBoxImage.Error); return; }
+
+            // TODO: Exercise 2: Task 3a: Check that the user has entered a valid date for the date of birth
+            if (!DateTime.TryParse(this.dateOfBirth.Text, out value)) { MessageBox.Show("The date of birth must be a valid date", "Error", MessageBoxButton.OK, MessageBoxImage.Error); return; }
+
+            // TODO: Exercise 2: Task 3b: Verify that the student is at least 5 years old
+            TimeSpan age = DateTime.Now.Subtract(value);
+            if (age.Days / 365 < 5) { MessageBox.Show("The student must be at least 5 years old", "Error", MessageBoxButton.OK, MessageBoxImage.Error); return; }
+            
             // Indicate that the data is valid
-            this.DialogResult = true;
+                this.DialogResult = true;
+            
         }
     }
 }
