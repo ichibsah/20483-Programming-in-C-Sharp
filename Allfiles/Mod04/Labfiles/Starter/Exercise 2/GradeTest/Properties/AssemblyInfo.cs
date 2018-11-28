81ebb30dc4b1665941c9c4513b42d54782284ce0
Ibrahim <ibrahim@sawadogo.xyz>
2018-11-28 16:28:42 +0100

    end of day

---
+++ Allfiles/Mod04/Labfiles/Starter/Exercise 2/GradeTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GradeTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GradeTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cdf55aaf-873c-4c65-acce-d9b821bdf617")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Allfiles/Mod06/Labfiles/Starter/Exercise 2/GradesPrototype/Views/HelperClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Linq;
+using Microsoft.Win32;
+using GradesPrototype.Controls;
+using GradesPrototype.Data;
+using GradesPrototype.Services;
+using Newtonsoft.Json;
+
+namespace GradesPrototype.Views
+{
+    internal class HelperClass
+    {
+        internal static void ReadFile(string gradesAsJson, out List<Grade> gradeList)
+        {
+            gradeList = JsonConvert.DeserializeObject<List<Grade>>(gradesAsJson);
+        }
+    }
+}
